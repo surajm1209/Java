e3a3efc52db82a1bb960e274f2653b68761e2b99
Surajm1209 <Surajm1209@gmail.com>
2023-10-02 11:25:20 +0530

    Array

---
+++ Array/Array_intro.java
@@ -0,0 +1,12 @@
+public class Intro_array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//create array
+		int [] ar=new int[0]; //create array
+		
+		int a=ar.length; //to count length of array
+		System.out.println(a);
+	}
+
+}
