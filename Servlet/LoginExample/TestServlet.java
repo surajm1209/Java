2c8b6d0d500a91d8afafe1b1807ffce059bcd5fb
Surajm1209 <Surajm1209@gmail.com>
2024-01-13 21:19:10 +0530

    Adding/Modify core servlet programs without IDE

---
+++ Servlet/LoginExample/TestServlet.java
@@ -0,0 +1,28 @@
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.*;
+
+@WebServlet(urlPatterns="/test")
+public class TestServlet extends HttpServlet
+{
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException
+	{
+		doProcess(request,response);
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException
+	{
+		doProcess(request,response);
+	}
+
+	public void doProcess(HttpServletRequest request,HttpServletResponse response)throws ServletException,IOException
+	{
+		System.out.println("Request method is of type :: "+request.getMethod());
+		String userName = request.getParameter("username");
+		System.out.println("username is :: "+userName);
+	}
+	
+}
